5d986a6afd5173779c0b947ad40fd908a4483c10
rasvanB <not.obelix@gmail.com>
2023-03-26 00:31:26 +0200

    fixed deprecated import

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,4 +1,4 @@
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import { type NextRequest } from "next/server";
 import { prisma } from "~/server/db";
 import { signUpSchema } from "~/types/authTypes";
