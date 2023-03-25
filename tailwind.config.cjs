770bd4389745a1b5709fe6468d97ce511f188ee1
rasvanB <not.obelix@gmail.com>
2023-03-25 23:13:18 +0200

    added inter font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
