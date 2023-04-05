42eb1591a6630872d80bfdf0e4b9aa99353c56d2
rasvanB <not.obelix@gmail.com>
2023-04-05 22:08:21 +0300

    custom hook for platform

---
+++ src/hooks/usePlatforms.tsx
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { type Account } from "next-auth";
+
+const fetchAccounts = async () => {
+  try {
+    const { data } = await axios.get<Account[]>(`/api/me/accounts`);
+    return data;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+
+const usePlatforms = () => useQuery(["platforms"], () => fetchAccounts());
+
+export default usePlatforms;
