32dd7456e594c35078908d61bef3528c2adcd8ba
rasvanB <not.obelix@gmail.com>
2023-04-17 05:25:38 +0300

    added react toast package

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,17 +1,14 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
-import { useAtom } from "jotai/react";
 import { type NextPage } from "next";
 import { useSession } from "next-auth/react";
+import { Toaster } from "react-hot-toast";
 import ButtonGroup from "~/components/button-group";
-import { SchedulePostModal, UserSettingsModal } from "~/components/modal";
+import ModalArea from "~/components/modal-area";
 import Nav from "~/components/nav";
 import Posts from "~/components/posts";
-import { createModalAtom, userSettingsAtom } from "~/utils/jotai";
 
 const App: NextPage = () => {
   const { data, status } = useSession();
-  const [createModalOpen, setCreateModalOpen] = useAtom(createModalAtom);
-  const [userSettingsOpen, setUserSettingsOpen] = useAtom(userSettingsAtom);
 
   if (status === "loading") {
     return <div>Loading...</div>;
@@ -23,18 +20,14 @@ const App: NextPage = () => {
 
   return (
     <div className="flex h-screen w-screen flex-col">
-      {createModalOpen && (
-        <SchedulePostModal onClose={() => setCreateModalOpen(false)} />
-      )}
-      {userSettingsOpen && (
-        <UserSettingsModal onClose={() => setUserSettingsOpen(false)} />
-      )}
+      <Toaster position="top-center" />
+      <ModalArea />
       <div className="px-2 sm:px-10 lg:px-40 xl:px-60">
         <Nav />
         <ButtonGroup />
       </div>
       <div className="mt-3 h-px w-full bg-[#DBDBDB]" />
-      <div className="h-full w-full bg-[#ECECEC]">
+      <div className="h-full w-full overflow-x-hidden bg-[#ECECEC] pb-5 pt-3">
         <Posts />
       </div>
     </div>
