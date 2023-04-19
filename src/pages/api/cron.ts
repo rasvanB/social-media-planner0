d222fd8278926def6cfa6e7ccb4ea0509238561c
rasvanB <not.obelix@gmail.com>
2023-04-19 22:39:20 +0300

    updated cron logic

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -3,14 +3,29 @@ import { prisma } from "~/server/db";
 
 const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
   const posts = await prisma.post.findMany({
-    where: { scheduledAt: { lte: new Date().getTime() } },
+    where: { scheduledAt: { lte: new Date().getTime() }, published: false },
     include: {
       platforms: true,
     },
   });
   // TODO: Send posts to platforms
   posts.forEach((post) => {
-    console.log(post);
+    const platforms = post.platforms.map((platform) => platform.name);
+
+    const user = prisma.user.findUnique({
+      where: { id: post.authorId },
+      include: { accounts: true },
+    });
+
+    if (platforms.includes("twitter")) {
+      // post to twitter
+    }
+    if (platforms.includes("facebook")) {
+      // post to facebook
+    }
+    if (platforms.includes("instagram")) {
+      // post to instagram
+    }
   });
 
   return res.status(200).json({ message: "OK" });
