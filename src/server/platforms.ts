a75c680f1a55f30ead796e4e720b65431ab8096a
rasvanB <not.obelix@gmail.com>
2023-04-21 20:26:46 +0300

    replaced twit logic with new api logic

--- src/server/platforms.ts
+++ src/server/platforms.ts
@@ -1,6 +1,5 @@
-import Twit from "twit";
-import { z } from "zod";
 import { env } from "~/env.mjs";
+import { TwitterApi } from "twitter-api-v2";
 
 type TwitterClientConfig = {
   access_token: string;
@@ -8,63 +7,25 @@ type TwitterClientConfig = {
 };
 
 export const createTwitterClient = (config: TwitterClientConfig) => {
-  return new Twit({
-    consumer_key: env.TWITTER_CLIENT_ID,
-    consumer_secret: env.TWITTER_CLIENT_SECRET,
-    ...config,
+  return new TwitterApi({
+    appKey: env.TWITTER_CLIENT_ID,
+    appSecret: env.TWITTER_CLIENT_SECRET,
+    accessToken: config.access_token,
+    accessSecret: config.access_token_secret,
   });
 };
 
-const dataSchema = z.object({
-  media_id_string: z.string(),
-});
-
 export class TwitterClient {
-  client: Twit;
+  client: TwitterApi;
 
   constructor(config: TwitterClientConfig) {
     this.client = createTwitterClient(config);
   }
 
-  postImagePost(content: string, b64content: string | Buffer) {
-    this.client.post(
-      "/media/upload",
-      {
-        media_data: b64content,
-      },
-      (err, data, _response) => {
-        if (err) {
-          console.log("ERROR:", err);
-          return;
-        }
-        const res = dataSchema.safeParse(data);
-        if (!res.success) {
-          console.log("ERROR:", res.error);
-          return;
-        }
-        const mediaIdStr = res.data.media_id_string;
-        const meta_params = { media_id: mediaIdStr };
-        this.client.post(
-          "/media/metadata/create",
-          meta_params,
-          (err, _data, _response) => {
-            if (!err) {
-              const params = { status: content, media_ids: [mediaIdStr] };
-              this.client.post(
-                "statuses/update",
-                params,
-                (err, data, _response) => {
-                  if (err) {
-                    console.log("ERROR:", err);
-                    return;
-                  }
-                  console.log("SUCCESS:", data);
-                }
-              );
-            }
-          }
-        );
-      }
-    );
+  async createImagePost(content: string) {
+    const media_id = await this.client.v1.uploadMedia("image.png");
+    await this.client.v1.tweet(content, {
+      media_ids: media_id,
+    });
   }
 }
