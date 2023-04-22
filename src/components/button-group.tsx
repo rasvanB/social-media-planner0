03a386e25a8d6ee04a959c63273b1694b4918fbe
rasvanB <not.obelix@gmail.com>
2023-04-22 14:20:45 +0300

    added sorting and filtering logic

--- src/components/button-group.tsx
+++ src/components/button-group.tsx
@@ -1,14 +1,19 @@
 import { useAtom } from "jotai";
 import { createModalAtom } from "~/utils/jotai";
 import { ScheduleButton } from "./button";
+import PlatformSelect from "./platform-select";
+import SortInput from "./sort-input";
 
 const ButtonGroup = () => {
   const [, setCreateModalOpen] = useAtom(createModalAtom);
 
   return (
-    <div className="mt-7 flex w-full justify-between">
+    <div className="mt-7 flex w-full flex-col flex-wrap justify-between gap-2 md:flex-row">
       <ScheduleButton onClick={() => setCreateModalOpen(true)} />
-      <div className="flex gap-2"></div>
+      <div className="flex flex-wrap gap-2">
+        <PlatformSelect />
+        <SortInput />
+      </div>
     </div>
   );
 };
--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -1,10 +1,14 @@
+import { useAtom } from "jotai";
 import { useSession } from "next-auth/react";
 import usePosts from "~/hooks/usePosts";
+import { selectedPlatformsAtom, sortingAtom } from "~/utils/jotai";
 import Post from "./post";
 
 const Posts = () => {
   const session = useSession();
   const { data, status } = usePosts();
+  const [selectedPlatforms] = useAtom(selectedPlatformsAtom);
+  const [sortingCriteria] = useAtom(sortingAtom);
 
   if (status === "loading" || !session || !session.data) {
     return (
@@ -24,20 +28,49 @@ const Posts = () => {
     );
   }
 
+  const filteredData = data.filter((post) => {
+    const postPlatforms = post.platforms.map((platform) => platform.name);
+
+    return selectedPlatforms.some((platform) =>
+      postPlatforms.includes(platform)
+    );
+  });
+
+  const sortedData = filteredData.sort((a, b) => {
+    switch (sortingCriteria.column) {
+      case "schedule":
+        return sortingCriteria.ascending
+          ? a.scheduledAt - b.scheduledAt
+          : b.scheduledAt - a.scheduledAt;
+      case "creation":
+        return sortingCriteria.ascending
+          ? a.createdAt - b.createdAt
+          : b.createdAt - a.createdAt;
+    }
+  });
+
   return (
     <div className="mt-2 px-2 sm:px-10 lg:px-40 xl:px-60">
       <div className="flex flex-col items-center gap-3">
-        {data.map((post) => (
-          <Post
-            key={post.id}
-            post={post}
-            user={{
-              id: session.data.user.id,
-              name: session.data.user.username,
-              image: session.data.user.image,
-            }}
-          />
-        ))}
+        {sortedData.length > 0 ? (
+          sortedData.map((post) => (
+            <Post
+              key={post.id}
+              post={post}
+              user={{
+                id: session.data.user.id,
+                name: session.data.user.username,
+                image: session.data.user.image,
+              }}
+            />
+          ))
+        ) : (
+          <div className="mt-3 flex flex-col items-center px-2 text-center text-xl font-semibold text-black/50">
+            {
+              "No posts matching your selected platforms. Try selecting different platforms."
+            }
+          </div>
+        )}
       </div>
     </div>
   );
