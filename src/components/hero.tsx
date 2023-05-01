64dbfbf8050c858d5513a81f31e676ad06554c21
rasvanB <not.obelix@gmail.com>
2023-05-01 23:14:41 +0300

    added subheading

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,16 +1,26 @@
 const Hero = () => {
   return (
     <div>
-      <div className="text-5xl font-medium even:ml-2">
-        Create once, post{" "}
-        <span
-          className="
-          bg-gradient-to-r from-blue-400 to-blue-600 bg-clip-text text-transparent
-        "
-        >
-          everywhere
+      <div className="mt-32 text-center text-6xl font-semibold text-black/90 even:ml-2">
+        Create once, <br />
+        <span>
+          post{" "}
+          <span className="bg-gradient-to-r from-blue-400 to-blue-600 bg-clip-text text-transparent">
+            everywhere
+          </span>
         </span>
         .
+      </div>
+      <div className="mt-6 text-center text-xl">
+        Stay Ahead of the Social Media Game:{" "}
+        <span className="text-blue-600 underline underline-offset-4">Plan</span>
+        ,{" "}
+        <span className="text-blue-600 underline underline-offset-4">
+          Schedule
+        </span>
+        , and{" "}
+        <span className="text-blue-600 underline underline-offset-4">Post</span>{" "}
+        with Wave
       </div>
     </div>
   );
